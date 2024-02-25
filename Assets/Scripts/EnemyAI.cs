01024a4ab302f60d7b1cf5c60e0edbb5bbe3070e
BlackWarn <146531111+BlackWarn@users.noreply.github.com>
2024-02-25 13:10:50 +0700

    Build_5

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,19 +8,38 @@ public class EnemyAI : MonoBehaviour
     public List<Transform> patrolPoints;
     public PlayerController player;
     public float viewAngle;
+    public float damage = 30;
+
     private NavMeshAgent _navMeshAgent;
     private bool _isPlayerNoticed;
+    private PlayerHealth _playerHealth;
 
     private void Start()
     {
         InitComponentLinks();
         PickNewPatrolPoint();
     }
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _playerHealth = player.GetComponent<PlayerHealth>();
+    }
     private void Update()
     {
         NoticePlayerUpdate();
         ChaseUpdate();
+        AttackUpdate();
         PatrolUpdate();
+    }
+    private void AttackUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            if(_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                _playerHealth.DealDamage(damage * Time.deltaTime);
+            }
+        }
     }
     private void NoticePlayerUpdate()
     {
@@ -46,16 +65,13 @@ public class EnemyAI : MonoBehaviour
     {
         if (!_isPlayerNoticed)
         {
-            if (_navMeshAgent.remainingDistance == 0)
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
             {
                 PickNewPatrolPoint();
             }
         }
     }
-    private void InitComponentLinks()
-    {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
-    }
+    
     private void ChaseUpdate()
     {
         if (_isPlayerNoticed)

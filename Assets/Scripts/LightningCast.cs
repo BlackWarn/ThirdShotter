7b346aaf97a7c8f7970f0b8257b16467cccbe6f1
BlackWarn <146531111+BlackWarn@users.noreply.github.com>
2024-03-02 12:45:39 +0700

    CA_Light

---
+++ Assets/Scripts/LightningCast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningCast : MonoBehaviour
+{
+    public float Damage = 1f;
+    public float Range = 100f;
+    public RaycastHit hit;
+    public Camera Mcamera;
+
+    private LineRenderer _lightning;
+    
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Casting();
+        }
+    }
+    public void Casting()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(Mcamera.transform.position, Mcamera.transform.forward, out hit, Range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
---
+++ Assets/Scripts/LightningLine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningLine : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public LightningCast _lightningCast;
+    public GameObject TargetPoint;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        _lightningCast = GetComponent<LightningCast>();
+    }
+    private void Update()
+    {
+        lineRenderer.SetPositions(new Vector3[] { TargetPoint.transform.position, _lightningCast.hit.point });
+    }
+
+}

807c10a7dfabbad58621bf6401821331b0888399
BlackWarn <146531111+BlackWarn@users.noreply.github.com>
2024-03-07 19:38:19 +0700

    Cyber Heart_Build_1

--- Assets/Scripts/LightningCast.cs
+++ Assets/Scripts/LightningCast.cs
@@ -6,24 +6,49 @@ public class LightningCast : MonoBehaviour
 {
     public float Damage = 1f;
     public float Range = 100f;
-    public RaycastHit hit;
+    public float timeStart = 5f;
+    public float timeFix = 5f;
+    public float delay = 0.6f;
     public Camera Mcamera;
+    public GameObject LigtningVisible;
 
     private LineRenderer _lightning;
     
-    public void Update()
+    private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Casting();
-        }
+        Timer();
+        LightningShoot();
+        //Debug.Log(timeStart);
     }
-    public void Casting()
+    private void Casting()
     {
         RaycastHit hit;
         if(Physics.Raycast(Mcamera.transform.position, Mcamera.transform.forward, out hit, Range))
         {
             Debug.Log(hit.transform.name);
         }
+    }
+    private void Timer()
+    {
+        if (timeStart >= 0)
+        {
+            timeStart -= Time.deltaTime;
+        }
+    }
+    private void LightningShoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (timeStart <= 0)
+            {
+                LigtningVisible.SetActive(true);
+                Invoke("LigtningNotVisible", delay);
+                timeStart = timeFix;
+            }
+        }
+    }
+    private void LigtningNotVisible()
+    {
+        LigtningVisible.SetActive(false);
     }
 }
